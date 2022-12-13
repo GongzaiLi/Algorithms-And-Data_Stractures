6542a0764a848ccc6674eb6da6138f828f11f056
Gongzai Li <gli65@uclive.ac.nz>
2022-12-14 10:36:03 +1300

    feat: update the test equal function

--- helper/utils.py
+++ helper/utils.py
@@ -1,2 +1,5 @@
-def testEqual(itemA, itemB):
-    print(itemA == itemB)
+def testEqual(answer, test_case):
+    if answer == test_case:
+        print("Pass")
+    else:
+        print(F"Expected | Got \n {answer} | {test_case}")
